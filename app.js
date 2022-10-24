f0cd77c48d667e5f175e4ce5db23999d591e92d1
VladislavOoll <apahiphip@mail.ru>
2022-10-24 21:57:09 +0300

    test: using enum

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 "use strict";
-const skill = [1, 'Dev'];
-const skills = ['Dev', 'DevOps'];
-console.log(skills);
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILD"] = 3] = "FAILD";
+})(StatusCode || (StatusCode = {}));
+var StatusCodeStr;
+(function (StatusCodeStr) {
+    StatusCodeStr["SUCCESS"] = "a";
+    StatusCodeStr["IN_PROCESS"] = "c";
+    StatusCodeStr["FAILD"] = "f";
+})(StatusCodeStr || (StatusCodeStr = {}));
+var StatusCodeGeter;
+(function (StatusCodeGeter) {
+    StatusCodeGeter[StatusCodeGeter["SUCCESS"] = 1] = "SUCCESS";
+    StatusCodeGeter["IN_PROCESS"] = "h";
+    StatusCodeGeter["FAILD"] = "f";
+})(StatusCodeGeter || (StatusCodeGeter = {}));
+const res = {
+    message: 'Платеж успешен',
+    status: StatusCode.SUCCESS
+};
+function action(status) {
+    console.log('res = ' + status);
+}
+const res2 = 1 /* Roles.ADMIN */;
+action(StatusCodeGeter.FAILD);
+console.log(res.status === StatusCode.SUCCESS);
--- app.ts
+++ app.ts
@@ -1,5 +1,37 @@
-const skill: readonly [number, string] = [1, 'Dev'];
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROCESS,
+    FAILD
+}
 
-const skills: ReadonlyArray<string> = ['Dev', 'DevOps'];
+enum StatusCodeStr {
+    SUCCESS = 'a',
+    IN_PROCESS = 'c',
+    FAILD = 'f'
+}
 
-console.log(skills);
+enum StatusCodeGeter {
+    SUCCESS = 1,
+    IN_PROCESS = 'h',
+    FAILD = 'f'
+}
+
+const res = {
+    message: 'Платеж успешен',
+    status: StatusCode.SUCCESS
+}
+
+function action(status: StatusCodeGeter) {
+    console.log('res = ' + status )
+}
+
+const enum Roles {
+    ADMIN = 1,
+    USER = 2
+}
+
+const res2 = Roles.ADMIN;
+
+action(StatusCodeGeter.FAILD)
+
+console.log(res.status === StatusCode.SUCCESS);
