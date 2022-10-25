928a1c362018facb60e9b60726408c1e2b00a586
VladislavOoll <apahiphip@mail.ru>
2022-10-25 23:17:12 +0300

    test: invisible const enum

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var QuestionStatus;
-(function (QuestionStatus) {
-    QuestionStatus["Published"] = "published";
-    QuestionStatus["Draft"] = "draft";
-    QuestionStatus["Deleted"] = "deleted";
-})(QuestionStatus || (QuestionStatus = {}));
-function getFaqs(req) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const res = yield fetch('/faqs', {
-            method: 'POST',
-            body: JSON.stringify(req)
-        });
-        const data = yield res.json();
-        return data;
-    });
-}
-/* Запрос */
-// {
-//     "topicId": 5,
-//     "status": "published" // "draft", "deleted"
-// }
-/* Ответ */
-// [
-//     {
-//         "question": "Как осуществляется доставка?",
-//         "answer": "быстро!",
-//         "tags": [
-//             "popular",
-//             "new"
-//         ],
-//         "likes": 3,
-//         "status": "published"
-//     }
-// ]
+var options;
+(function (options) {
+    options["yes"] = "yes";
+    options["no"] = "no";
+})(options || (options = {}));
+const b = 2323;
+const fun = (opt) => console.log(opt + opt);
+fun(options.yes);
--- app.ts
+++ app.ts
@@ -1,50 +1,15 @@
-enum QuestionStatus {
-    Published = 'published',
-    Draft = 'draft',
-    Deleted = 'deleted'
+enum options {
+    yes = 'yes',
+    no = 'no'
 }
 
-async function getFaqs(
-    req: {
-        topicId: number;
-        status?: QuestionStatus;
-    }): Promise<{
-    question: string;
-    answer: string;
-    tags: string[];
-    likes: number;
-    status: QuestionStatus;
-}[]> {
-    const res = await fetch('/faqs', {
-        method: 'POST',
-        body: JSON.stringify(req)
-    });
-    const data: {
-        question: string;
-        answer: string;
-        tags: string[];
-        likes: number;
-        status: QuestionStatus;
-    }[] = await res.json();
-    return data;
+const enum options2 {
+    yes = 'yes',
+    no = 'no'
 }
 
+const b = 2323;
 
-/* Запрос */
-// {
-//     "topicId": 5,
-//     "status": "published" // "draft", "deleted"
-// }
-/* Ответ */
-// [
-//     {
-//         "question": "Как осуществляется доставка?",
-//         "answer": "быстро!",
-//         "tags": [
-//             "popular",
-//             "new"
-//         ],
-//         "likes": 3,
-//         "status": "published"
-//     }
-// ]
+const fun = (opt) => console.log(opt + opt);
+
+fun(options.yes)
