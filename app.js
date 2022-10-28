c764df1a46290cd368d5b6f7e398c44a2337255a
VladislavOoll <apahiphip@mail.ru>
2022-10-28 23:56:13 +0300

    test: literal string and number types

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 "use strict";
+function fetchWidthAuth(url, method) {
+    console.log(method);
+    return -1;
+}
+const a = 'sasd';
+let b = 'dsfsdkasd';
+fetchWidthAuth('s', 'post');
+const method = 'post';
+fetchWidthAuth('s', method);
+let method2 = 'post';
+fetchWidthAuth('s', method2);
+let method3 = 'post';
+fetchWidthAuth('s', method3);
--- app.ts
+++ app.ts
@@ -1,3 +1,20 @@
-function fetchWidthAuth(url: string, method: 'post' | 'get') {
+function fetchWidthAuth(url: string, method: 'post' | 'get'): 1 | -1 {
     console.log(method)
+    return -1;
 }
+
+const a = 'sasd';
+let b: 'dsfsdkasd' = 'dsfsdkasd';
+
+fetchWidthAuth('s', 'post');
+
+const method = 'post';
+fetchWidthAuth('s', method);
+
+let method2: 'post' = 'post';
+fetchWidthAuth('s', method2);
+
+let method3 = 'get';
+fetchWidthAuth('s', method3 as 'get');
+
+
