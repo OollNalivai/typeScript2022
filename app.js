b247a675f43f21fcb71002d469c72a2ab8a63cad
VladislavOoll <apahiphip@mail.ru>
2022-10-29 23:58:22 +0300

    test: interface

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 "use strict";
-function fetchWidthAuth(url, method) {
-    console.log(method);
-    return -1;
-}
 let use = {
     name: 'Poly',
     age: 22,
     skills: ['1', '2'],
-    id: 2
+    roleId: 1,
+    createdAt: new Date(),
+    log(id) {
+        return '';
+    }
 };
--- app.ts
+++ app.ts
@@ -1,30 +1,37 @@
-type httpMethod = 'post' | 'get';
-
-function fetchWidthAuth(url: string, method: httpMethod): 1 | -1 {
-    console.log(method);
-    return -1;
-}
-
-type User = {
+interface User {
     name: string,
     age: number,
     skills: string[]
+
+    log: (id: number) => string
 }
 
-type Role = {
-    name: string
-    id: number
+interface Role {
+    roleId: number;
 }
 
-type UserWithRole = User & Role;
+interface UserWithRole extends User, Role {
+    createdAt: Date
+}
+
 let use: UserWithRole = {
     name: 'Poly',
     age: 22,
     skills: ['1', '2'],
-    id: 2
+    roleId: 1,
+    createdAt: new Date(),
+
+    log(id) {
+        return '';
+    }
 };
 
-type UserWithRole_2 = {
-    user: User,
-    role: Role
+interface UserDic {
+    [index: number]: User
 }
+
+type UserDic2 = {
+    [index: number]: User
+}
+
+type ud = Record<number, User>
