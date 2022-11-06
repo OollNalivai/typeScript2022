dc94a54dc623bbd71190ce2ab19411c5cb661b48
VladislavOoll <apahiphip@mail.ru>
2022-11-06 19:08:08 +0300

    test: never (path 1)

--- app.ts
+++ app.ts
@@ -1,38 +1,15 @@
-let input: unknown;
-
-input = 3;
-input = ['asd', 'asd'];
-
-function run(i: unknown) {
-    if (typeof i == 'number') {
-        i++;
-    } else {
-        i;
-    }
-    return console.log(i);
+function generateError(message: string): never {
+    throw new Error(message)
 }
 
-run(input);
-
-async function getData() {
-    try {
-        await fetch('');
-    } catch (error) {
-        if (error instanceof Error) {
-            console.log(error.message);
-        }
-    }
+function dumpError(): never {
+    while (true) {}
 }
 
-async function getDataForce() {
-    try {
-        await fetch('');
-    } catch (error) {
-        const e = error as Error;
-        console.log(e.message); // только если 100% понимаешь .что ошибка, иначе код упадет
-    }
+function rec(): never {
+    return rec()
 }
 
-type U1 = unknown | number
+const a0: void = undefined
 
-type I1 = unknown & string
+let a: never;
