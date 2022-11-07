ac9323620847f353eaddb7dc3eb2f8eb3e3ff2c5
VladislavOoll <apahiphip@mail.ru>
2022-11-07 15:31:18 +0300

    test: never switch case

--- app.ts
+++ app.ts
@@ -1,15 +1,32 @@
 function generateError(message: string): never {
-    throw new Error(message)
+    throw new Error(message);
 }
 
 function dumpError(): never {
-    while (true) {}
+    while (true) {
+    }
 }
 
 function rec(): never {
-    return rec()
+    return rec();
 }
 
-const a0: void = undefined
+const a0: void = undefined;
 
 let a: never;
+
+type paymentAction = 'refund' | 'checkout';
+
+function processAction(action: paymentAction) {
+    switch (action) {
+        case 'refund':
+            //...
+            break;
+        case 'checkout':
+            //...
+            break;
+        default:
+            const _: never = action;
+            throw new Error('Нет такого action');
+    }
+}
