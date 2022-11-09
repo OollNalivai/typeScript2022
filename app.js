f1c67323c11f3ca0da14e258f7c8610c523e21a8
VladislavOoll <apahiphip@mail.ru>
2022-11-09 18:57:09 +0300

    test: type conversion

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 "use strict";
-const n = null;
-const n1 = null;
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null;
-    }
-    else {
-        return {
-            name: 'Alashey'
-        };
-    }
-}
-const user = getUser();
-if (user) {
-    const n6 = user.name;
+let a = 5;
+let b = a.toString();
+let e = new String(a).valueOf();
+let f = new Boolean(a).valueOf();
+let c = 'asd';
+let d = +c;
+let d1 = parseInt(c);
+const user = {
+    name: 'adasdas',
+    email: 'asd@asd.asd',
+    login: 'asdad'
+};
+const admin = Object.assign(Object.assign({}, user), { role: 1 });
+function userToAdmin(user) {
+    return {
+        name: user.name,
+        role: 1
+    };
 }
--- app.ts
+++ app.ts
@@ -1,22 +1,39 @@
-const n: null = null;
-const n1: any = null;
+let a = 5;
+let b: string = a.toString();
+
+let e: string = new String(a).valueOf();
+let f: boolean = new Boolean(a).valueOf();
+
+let c = 'asd';
+let d: number = +c;
+let d1: number = parseInt(c);
+
 
 interface User {
     name: string;
+    email: string;
+    login: string;
 }
 
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null;
-    } else {
-        return {
-            name: 'Alashey'
-        } as User;
+const user: User = {
+    name: 'adasdas',
+    email: 'asd@asd.asd',
+    login: 'asdad'
+}
+
+interface Admin {
+    name: string,
+    role: number
+}
+
+const admin: Admin = {
+    ...user,
+    role: 1
+}
+
+function userToAdmin (user: User): Admin {
+    return {
+        name: user.name,
+        role: 1
     }
-}
-
-const user = getUser();
-
-if (user) {
-    const n6 = user.name;
 }
