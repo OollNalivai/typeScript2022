dbe5f04a0089659b48b5225bf7a02149188e09ff
VladislavOoll <apahiphip@mail.ru>
2022-11-26 20:35:19 +0300

    test: visibility of the property (protected, #private)

--- app.ts
+++ app.ts
@@ -2,13 +2,20 @@ class Vehicle {
     public make: string;
     private damages: string[] = [];
     private _model: string;
+    protected run: number;
+    #price: number;
 
     set model(m: string) {
         this._model = m;
+        this.#price = 100;
     }
 
     get model() {
         return this._model;
+    }
+
+    isPriceEquel (v: Vehicle) {
+        return this.#price === v.#price
     }
 
     addDamage(damage: string) {
@@ -17,6 +24,14 @@ class Vehicle {
         console.log(this.damages);
     }
 }
+
+class EuroTruck extends Vehicle {
+    setRun(km: number) {
+        this.run = km / 0.62;
+        // this.damages - error
+    }
+}
+
 
 new Vehicle().addDamage('222222');
 let b = new Vehicle();
