43eecf7efba6fbb8a069eff9f30ead5402855d8c
VladislavOoll <apahiphip@mail.ru>
2022-11-27 16:52:28 +0300

    test: task class Cart (part 1)

--- app.ts
+++ app.ts
@@ -1,43 +1,33 @@
-class Vehicle {
-    public make: string;
-    private damages: string[] = [];
-    private _model: string;
-    protected run: number;
-    #price: number;
-
-    set model(m: string) {
-        this._model = m;
-        this.#price = 100;
-    }
-
-    get model() {
-        return this._model;
-    }
-
-    isPriceEquel (v: Vehicle) {
-        return this.#price === v.#price
-    }
-
-    addDamage(damage: string) {
-        this.damages.push(damage);
-
-        console.log(this.damages);
+class Product {
+    constructor(
+        id: number,
+        title: string,
+        price: number
+    ) {
     }
 }
 
-class EuroTruck extends Vehicle {
-    setRun(km: number) {
-        this.run = km / 0.62;
-        // this.damages - error
+class Delivery {
+    date: Date;
+    address: string;
+    shopId: number;
+
+    constructor(date: Date, address: string);
+    constructor(date: Date, shopId: number);
+    constructor(date: Date, addressOrShopId?: string | number) {
+        this.date = date
+        if (typeof addressOrShopId === 'string') {
+            this.address = addressOrShopId;
+        } else if (typeof addressOrShopId === 'number') {
+            this.shopId = addressOrShopId;
+        }
     }
 }
 
+const user1 = new Delivery(new Date(), 'asdasd');
+const user2 = new Delivery(new Date(),22);
 
-new Vehicle().addDamage('222222');
-let b = new Vehicle();
-b.model = '1111111';
-if (b.model) {
-    console.log(b.model);
+class Cart {
+    products: any;
+    delivery: any;
 }
-
-
