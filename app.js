2943629d2df9ea69fdc5f93e05308cedd7409ce1
VladislavOoll <apahiphip@mail.ru>
2022-12-03 23:49:23 +0300

    test: static

--- app.js
+++ app.js
@@ -1,56 +1,17 @@
 "use strict";
-class Product {
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
+class UserService {
+    constructor(id) {
+    }
+    static getUser(id) {
+        return this.db.finedById(id);
+    }
+    create() {
+        UserService.db = {};
     }
 }
-class Delivery {
-    constructor(addressOrShopId, dateDelivery = new Date()) {
-        this.dateDelivery = dateDelivery;
-        if (typeof addressOrShopId === 'string') {
-            this.address = addressOrShopId;
-        }
-        else if (typeof addressOrShopId === 'number') {
-            this.shopId = addressOrShopId;
-        }
-    }
-}
-class Cart {
-    constructor() {
-        this.products = [];
-    }
-    addProduct(product) {
-        this.products.push(product);
-    }
-    deleteProductById(id) {
-        this.products = this.products.filter((product) => product.id !== id);
-        console.log(this.products);
-    }
-    getSum() {
-        return this.products.reduce((acc, cur) => acc + cur.price, 0);
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkOut() {
-        if (this.products.length === 0) {
-            throw new Error('Нет продуктов, привезти пустой пакет?');
-        }
-        if (this.delivery === undefined) {
-            throw new Error('Нет данных по доставке, рил, куда везти?');
-        }
-        console.log('Плати деньги, еда выезжает.');
-        return { success: true };
-    }
-}
-const cart = new Cart();
-cart.addProduct(new Product(0, '21312', 20));
-cart.addProduct(new Product(1, '21312', 20));
-cart.addProduct(new Product(2, '21312', 20));
-cart.addProduct(new Product(3, '21312', 20));
-cart.deleteProductById(2);
-console.log(cart.getSum());
-cart.setDelivery(new Delivery('234234', new Date()));
-cart.checkOut();
+(() => {
+    UserService.db = '123';
+})();
+UserService.getUser(1);
+const inst = new UserService(1);
+inst.create();
--- app.ts
+++ app.ts
@@ -1,77 +1,23 @@
-class Product {
-    id: number;
-    title: string;
-    price: number;
+class UserService {
+    // static  name: string = '123123';
+    private static db: any;
 
-    constructor(
-        id: number,
-        title: string,
-        price: number
-    ) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
+    static getUser(id: number) {
+        return this.db.finedById(id);
+    }
+
+    constructor(id: number) {
+    }
+
+    create() {
+        UserService.db = {};
+    }
+
+    static {
+        UserService.db = '123';
     }
 }
 
-class Delivery {
-    private dateDelivery: Date;
-    private address?: string;
-    private shopId?: number;
-
-    constructor(address: string, dateDelivery: Date);
-    constructor(shopId: number);
-    constructor(addressOrShopId: string | number, dateDelivery: Date = new Date()) {
-        this.dateDelivery = dateDelivery;
-        if (typeof addressOrShopId === 'string') {
-            this.address = addressOrShopId;
-        } else if (typeof addressOrShopId === 'number') {
-            this.shopId = addressOrShopId;
-        }
-    }
-}
-
-class Cart {
-    private products: Product[] = [];
-    private delivery: Delivery;
-
-    addProduct(product: Product): void {
-        this.products.push(product);
-    }
-
-    deleteProductById(id: number): void {
-        this.products = this.products.filter((product: Product) => product.id !== id);
-        console.log(this.products);
-    }
-
-    getSum(): number {
-        return this.products.reduce((acc, cur) => acc + cur.price, 0);
-    }
-
-    setDelivery(delivery: Delivery): void {
-        this.delivery = delivery;
-    }
-
-    checkOut() {
-        if (this.products.length === 0) {
-            throw new Error('Нет продуктов, привезти пустой пакет?');
-        }
-
-        if (this.delivery === undefined) {
-            throw new Error('Нет данных по доставке, рил, куда везти?');
-        }
-
-        console.log('Плати деньги, еда выезжает.');
-        return { success: true };
-    }
-}
-
-const cart = new Cart();
-cart.addProduct(new Product(0, '21312', 20));
-cart.addProduct(new Product(1, '21312', 20));
-cart.addProduct(new Product(2, '21312', 20));
-cart.addProduct(new Product(3, '21312', 20));
-cart.deleteProductById(2);
-console.log(cart.getSum());
-cart.setDelivery(new Delivery('234234', new Date()));
-cart.checkOut();
+UserService.getUser(1);
+const inst = new UserService(1);
+inst.create();
