f4e3fbef642a77d5d4d5429b34b28d4e7cfbd367
VladislavOoll <apahiphip@mail.ru>
2022-12-11 20:55:19 +0300

    test: ts config strict type checking, take into account `null` and `undefined`

--- build/app.js
+++ build/app.js
@@ -1,17 +1,6 @@
 "use strict";
-class Logger {
-    printDate(date) {
-        this.log(date.toString());
+function test(a) {
+    if (a > 0) {
+        return a;
     }
 }
-class MyLogger extends Logger {
-    log(message) {
-        console.log('printDate message: ', message);
-    }
-    logWithDate(message) {
-        this.printDate(new Date());
-        this.log(message);
-    }
-}
-const logger = new MyLogger();
-logger.logWithDate('time > on time');
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,5 @@
-// import {} from '@lib/';
-
-abstract class Logger {
-    abstract log(message: string): void;
-
-    printDate(date: Date): void {
-        this.log(date.toString());
+function  test(a: number): number | undefined {
+    if(a > 0) {
+        return a;
     }
 }
-
-class MyLogger extends Logger {
-
-    log(message: string): void {
-        console.log('printDate message: ', message);
-    }
-
-    logWithDate(message: string): void {
-        this.printDate(new Date());
-        this.log(message);
-    }
-}
-
-const logger = new MyLogger()
-logger.logWithDate('time > on time')
