61be0da570da53c481e53b32fa3137f1083277ca
VladislavOoll <apahiphip@mail.ru>
2023-01-02 21:02:12 +0300

    test: Conditional Types more

--- build/app.js
+++ build/app.js
@@ -1,2 +1,32 @@
 "use strict";
 const a1 = Math.random() > 0.5 ? 1 : 0;
+const suc = {
+    code: 200,
+    data: 'done'
+};
+const err = {
+    code: 404,
+    data: new Error()
+};
+class User {
+}
+class UserPersistent extends User {
+}
+function getUser(dbIdOrId) {
+    if (typeof dbIdOrId === 'number') {
+        return new User();
+    }
+    else {
+        return new UserPersistent();
+    }
+}
+function getUser2(id) {
+    if (typeof id === 'number') {
+        return new User();
+    }
+    else {
+        return new UserPersistent();
+    }
+}
+const res = getUser2(1);
+const res2 = getUser2('123123');
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,50 @@ const a1: number = Math.random() > 0.5 ? 1 : 0;
 interface HTTPPResponse<T extends 'success' | 'failed'> {
     code: number;
     data: T extends 'success' ? string : Error;
-    data2: T extends 'success' ? string : number;
+    data2?: T extends 'success' ? string : number;
 
     // additionalData: string | number;
 }
 
+const suc: HTTPPResponse<'success'> = {
+    code: 200,
+    data: 'done'
+};
 
+const err: HTTPPResponse<'failed'> = {
+    code: 404,
+    data: new Error()
+};
+
+class User {
+    id: number;
+    name: string;
+}
+
+class UserPersistent extends User{
+    dbId: string;
+}
+
+function getUser(id: number): User;
+function getUser(dbId: string): UserPersistent ;
+function getUser(dbIdOrId: string | number): User | UserPersistent {
+    if (typeof dbIdOrId === 'number') {
+        return new User();
+    } else {
+        return new UserPersistent();
+    }
+}
+
+type UserOrUserPersistent<T extends string | number> =
+    T extends number ? User : UserPersistent;
+
+function getUser2<T extends string | number>(id: T): UserOrUserPersistent<T> {
+    if (typeof id === 'number') {
+        return new User() as UserOrUserPersistent<T>;
+    } else {
+        return new UserPersistent() as UserOrUserPersistent<T>;
+    }
+}
+
+const res = getUser2(1);
+const res2 = getUser2('123123');
