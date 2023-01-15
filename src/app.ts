9d7ef646884c69878da375c82fde3528e539e0d7
VladislavOoll <apahiphip@mail.ru>
2023-01-15 16:57:58 +0300

    test: awaited

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-class User {
-    constructor(public id: number, public name: string) {
-    }
+type A = Awaited<Promise<string>>
+type A2 = Awaited<Promise<Promise<string>>>
+
+interface IMenu {
+    name: string;
+    url: string;
 }
 
-type CP = ConstructorParameters<typeof User> //  [id: number, name: string ]
-type IT = InstanceType<typeof User> // User
+async function getMenu(): Promise<IMenu[]> {
+    return [{name: 'Analitic', url: 'analitics'}];
+}
+
+type R = Awaited<ReturnType<typeof getMenu>>
+
+async function getArray<T>(x: T): Promise<Awaited<T>[]> {
+    return [await x];
+}
+
+async function getArray2<T>(x: T): Promise<Awaited<T[]>> {
+    return [await x];
+}
