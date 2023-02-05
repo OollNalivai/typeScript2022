d00b5cc115a10fab4d63571f333deec128475bb8
VladislavOoll <apahiphip@mail.ru>
2023-02-05 22:47:46 +0300

    test: Import & export

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
-import { A } from './module/app2.js'
+import run, { a, type MyType2 } from './module/app2';
+import running from './module/app2';
+import * as all from './module/app2';
+import { Test as MyTest } from './module/app2';
+import type { MyType } from './module/app2';
 
-console.log(A.a);
+
+running();
+run();
+new MyTest;
+console.log(a);
+console.log(all.Test);
--- src/module/app2.ts
+++ src/module/app2.ts
@@ -1,7 +1,17 @@
-export namespace A {
-    export const a = 5;
+export const a = 5;
 
-    export interface B {
-        c: number
-    }
+export class Test {
 }
+
+export const Obj = {};
+
+export default function run() {
+    console.log('run');
+}
+
+export interface B {
+    c: number;
+}
+
+export type MyType = string | number
+export type MyType2 = string | number
