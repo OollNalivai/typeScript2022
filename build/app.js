60069ec7a090e9ce51beb7f69620eafb10ecd2d2
VladislavOoll <apahiphip@mail.ru>
2023-02-18 23:36:51 +0300

    test: pattern Adapter init

--- build/app.js
+++ build/app.js
@@ -1,39 +1,18 @@
 "use strict";
-class Notify {
-    send(template, to) {
-        console.log(`Отправляю ${template}: ${to}`);
-    }
-}
-class Log {
-    log(message) {
-        console.log(message);
-    }
-}
-class Template {
+class KVDatabase {
     constructor() {
-        this.template = [
-            { name: 'other', template: '<h1>Hello World!</h1>' }
-        ];
+        this.db = new Map();
     }
-    getByName(name) {
-        return this.template.find(t => t.name === name);
+    save(key, value) {
+        this.db.set(key, value);
     }
 }
-class NotificationFacade {
-    constructor() {
-        this.notify = new Notify();
-        this.template = new Template();
-        this.logger = new Log();
-    }
-    send(to, templateName) {
-        const data = this.template.getByName(templateName);
-        if (!data) {
-            this.logger.log('Не найден шаблон');
-            return;
-        }
-        this.notify.send(data.template, to);
-        this.logger.log('Шаблон отправлен');
+class PersistentDB {
+    savePersistent(_) {
     }
 }
-const s = new NotificationFacade();
-s.send('a@d.sa', 'other');
+class PersistentDBAdapter {
+}
+function run(base) {
+    base.save('key', 'myValue');
+}
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,20 @@
-class Notify {
-    send(template: string, to: string) {
-        console.log(`Отправляю ${template}: ${to}`);
+class KVDatabase {
+    private db: Map<string, string> = new Map();
+    save(key: string, value: string) {
+        this.db.set(key, value)
     }
 }
 
-class Log {
-    log(message: string) {
-        console.log(message);
+class PersistentDB {
+    savePersistent(_: Object) {
+        //
     }
 }
 
-class Template {
-    private template = [
-        {name: 'other', template: '<h1>Hello World!</h1>'}
-    ];
+class PersistentDBAdapter {
 
-    getByName(name: string) {
-        return this.template.find(t => t.name === name);
-    }
 }
 
-class NotificationFacade {
-    private notify: Notify;
-    private logger: Log;
-    private template: Template;
-
-    constructor() {
-        this.notify = new Notify();
-        this.template = new Template();
-        this.logger = new Log();
-    }
-
-    send(to: string, templateName: string) {
-        const data = this.template.getByName(templateName);
-        if (!data) {
-            this.logger.log('Не найден шаблон');
-            return;
-        }
-        this.notify.send(data.template, to);
-        this.logger.log('Шаблон отправлен');
-    }
+function run(base: KVDatabase) {
+    base.save('key', 'myValue')
 }
-
-const s = new NotificationFacade();
-s.send('a@d.sa', 'other')
