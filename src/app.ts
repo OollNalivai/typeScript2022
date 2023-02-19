0190045fac3fd0609c55cbc3af8d86057ebb2114
VladislavOoll <apahiphip@mail.ru>
2023-02-19 22:32:15 +0300

    test: pattern Proxy

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,36 @@
-class KVDatabase {
-    private db: Map<string, string> = new Map();
-    save(key: string, value: string) {
-        this.db.set(key, value)
+interface PaymentAPIInterface {
+    getPaymentDetail(id: number): PaymentDetailInterface | undefined;
+}
+
+interface PaymentDetailInterface {
+    id: number;
+    sum: number;
+}
+
+class PaymentAPI implements PaymentAPIInterface {
+    private data = [{id: 1, sum: 10000}];
+
+    getPaymentDetail(id: number): PaymentDetailInterface | undefined {
+        return this.data.find(d => d.id === id);
     }
 }
 
-class PersistentDB {
-    savePersistent(data: Object) {
-        console.log(data);
+class PaymentAccessProxy implements PaymentAPIInterface {
+    constructor(private api: PaymentAPI, private UserId: number) {
+    }
+
+    getPaymentDetail(id: number): PaymentDetailInterface | undefined {
+        if (this.UserId === 1) {
+            return this.api.getPaymentDetail(id);
+        }
+        console.log('Попытка получить данные платежа!');
+        return undefined;
     }
 }
 
-class PersistentDBAdapter extends KVDatabase {
-    constructor(public database: PersistentDB) {
-        super();
-    }
+const proxy = new PaymentAccessProxy(new PaymentAPI(), 1);
+console.log(proxy.getPaymentDetail(1));
 
-     override save(key: string, value: string): void {
-         this.database.savePersistent({key, value})
-     }
-}
 
-function runR(base: KVDatabase) {
-    base.save('key', 'myValue')
-}
-
-runR(new PersistentDBAdapter(new PersistentDB))
+const proxy2 = new PaymentAccessProxy(new PaymentAPI(), 2);
+console.log(proxy2.getPaymentDetail(1));
