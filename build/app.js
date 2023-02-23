b8e455e66d645d7519dc2b5b3a3a345ee878e49c
VladislavOoll <apahiphip@mail.ru>
2023-02-23 21:13:47 +0300

    test: pattern composite done

--- build/app.js
+++ build/app.js
@@ -1,9 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DeliveryShop = void 0;
+exports.Product = exports.Package = exports.DeliveryShop = void 0;
 class DeliveryItem {
+    constructor() {
+        this.items = [];
+    }
     addItem(item) {
         this.items.push(item);
+    }
+    getItemPrices() {
+        return this.items
+            .reduce((acc, i) => acc += i.getPrice(), 0);
     }
 }
 class DeliveryShop extends DeliveryItem {
@@ -12,7 +19,32 @@ class DeliveryShop extends DeliveryItem {
         this.deliveryFee = deliveryFee;
     }
     getPrice() {
-        return this.items.reduce((acc, i) => acc += i.getPrice(), 0) + this.deliveryFee;
+        return this.getItemPrices() + this.deliveryFee;
     }
 }
 exports.DeliveryShop = DeliveryShop;
+class Package extends DeliveryItem {
+    getPrice() {
+        return this.getItemPrices();
+    }
+}
+exports.Package = Package;
+class Product extends DeliveryItem {
+    constructor(price) {
+        super();
+        this.price = price;
+    }
+    getPrice() {
+        return this.price;
+    }
+}
+exports.Product = Product;
+const shop = new DeliveryShop(100);
+shop.addItem(new Product(1000));
+const pack1 = new Package();
+pack1.addItem(new Product(200));
+pack1.addItem(new Product(300));
+shop.addItem(pack1);
+const pack2 = new Package();
+pack2.addItem(new Product(30));
+console.log(shop.getPrice());
