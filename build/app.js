a648fed8b564f624b2f6f30dec28c880c12ed13f
VladislavOoll <apahiphip@mail.ru>
2023-02-25 16:28:38 +0300

    test: pattern Chain of Command abstract class AbstractMiddleware

--- build/app.js
+++ build/app.js
@@ -1,1 +1,13 @@
 "use strict";
+class AbstractMiddleware {
+    next(mid) {
+        this.nextMiddleware = mid;
+        return mid;
+    }
+    handle(request) {
+        if (this.nextMiddleware) {
+            return this.nextMiddleware.handle(request);
+        }
+        return;
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,19 @@ interface MiddlewareInterface {
     handle(request: any): any;
 }
 
-// abstract class AbstractMiddleware implements MiddlewareInterface {
-//
-//     next(mid: MiddlewareInterface): MiddlewareInterface {
-//         return undefined;
-//     }
-//
-//     handle(request: any): any {
-//     }
-// }
+abstract class AbstractMiddleware implements MiddlewareInterface {
+
+    private nextMiddleware: MiddlewareInterface;
+
+    next(mid: MiddlewareInterface): MiddlewareInterface {
+        this.nextMiddleware = mid;
+        return mid;
+    }
+
+    handle(request: any): any {
+        if (this.nextMiddleware) {
+            return this.nextMiddleware.handle(request);
+        }
+        return;
+    }
+}
