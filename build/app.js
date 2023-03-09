799ee7f659500f1061fa37df2e2be2b8ce449f6b
VladislavOoll <apahiphip@mail.ru>
2023-03-09 22:39:55 +0300

    test: pattern Iterator class PriorityTaskIterator

--- build/app.js
+++ build/app.js
@@ -8,4 +8,21 @@ class TaskList {
     addTask(task) {
         this.tasks.push(task);
     }
+    getTasks() {
+        return this.tasks;
+    }
+    count() {
+        return this.tasks.length;
+    }
+}
+class PriorityTaskIterator {
+    current() {
+        return undefined;
+    }
+    next() {
+        return undefined;
+    }
+    prev() {
+        return undefined;
+    }
 }
--- src/app.ts
+++ src/app.ts
@@ -9,4 +9,39 @@ class TaskList {
     addTask(task: Task) {
         this.tasks.push(task);
     }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    count() {
+        return this.tasks.length;
+    }
+}
+
+interface IteratorInterface<T> {
+    current(): T | undefined;
+
+    next(): T | undefined;
+
+    prev(): T | undefined;
+
+    index: number;
+}
+
+class PriorityTaskIterator implements IteratorInterface<Task> {
+    index: number;
+
+    current(): Task | undefined {
+        return undefined;
+    }
+
+    next(): Task | undefined {
+        return undefined;
+    }
+
+    prev(): Task | undefined {
+        return undefined;
+    }
+
 }
