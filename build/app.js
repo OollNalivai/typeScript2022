10ec4fe1b546282555fe4c5de7488e4a6a68ac77
VladislavOoll <apahiphip@mail.ru>
2023-03-15 22:42:29 +0300

    test: pattern Template Method init

--- build/app.js
+++ build/app.js
@@ -1,59 +1,18 @@
 "use strict";
-class Task {
-    constructor(priority) {
-        this.priority = priority;
+class Form {
+    constructor(name) {
+        this.name = name;
     }
 }
-class TaskList {
-    constructor() {
-        this.tasks = [];
+class SaveForm {
+    save(form) {
+        const res = this.fill(form);
+        this.log(res);
+        this.send(res);
     }
-    sortByPriority() {
-        this.tasks = this.tasks.sort((a, b) => {
-            return a.priority !== b.priority ? (a.priority > b.priority ? 1 : -1) : 0;
-        });
-    }
-    addTask(task) {
-        this.tasks.push(task);
-    }
-    getTasks() {
-        return this.tasks;
-    }
-    count() {
-        return this.tasks.length;
-    }
-    getIterator() {
-        return new PriorityTaskIterator(this);
+    log(data) {
+        console.log(data);
     }
 }
-class PriorityTaskIterator {
-    constructor(taskList) {
-        this.position = 0;
-        taskList.sortByPriority();
-        this.taskList = taskList;
-    }
-    current() {
-        return this.taskList.getTasks()[this.position];
-    }
-    next() {
-        this.position += 1;
-        return this.taskList.getTasks()[this.position];
-    }
-    prev() {
-        this.position -= 1;
-        return this.taskList.getTasks()[this.position];
-    }
-    index() {
-        return this.position;
-    }
+class FirstAPI {
 }
-const taskList = new TaskList();
-taskList.addTask(new Task(8));
-taskList.addTask(new Task(1));
-taskList.addTask(new Task(3));
-const iterator = taskList.getIterator();
-console.log(iterator.current());
-console.log(iterator.next());
-console.log(iterator.next());
-console.log(iterator.prev());
-console.log(iterator.index());
--- src/app.ts
+++ src/app.ts
@@ -1,85 +1,24 @@
-class Task {
-    constructor(public priority: number) {
+class Form {
+    constructor(public name: string) {
     }
 }
 
-class TaskList {
-    private tasks: Task[] = [];
-
-    sortByPriority() {
-        this.tasks = this.tasks.sort((a, b) => {
-            // if (a.priority > b.priority) {
-            //     return 1;
-            // } else {
-            //     return -1
-            // }
-            return a.priority !== b.priority ? (a.priority > b.priority ? 1 : -1) : 0;
-        });
+abstract class SaveForm<T> {
+    save(form: Form) {
+        const res = this.fill(form);
+        this.log(res);
+        this.send(res);
     }
 
-    addTask(task: Task) {
-        this.tasks.push(task);
+    protected abstract fill(form: Form): T;
+
+    protected log(data: T): void {
+        console.log(data);
     }
 
-    getTasks() {
-        return this.tasks;
-    }
-
-    count() {
-        return this.tasks.length;
-    }
-
-    getIterator() {
-        return new PriorityTaskIterator(this)
-    }
+    protected abstract send(data: T): void
 }
 
-interface IteratorInterface<T> {
-    current(): T | undefined;
+class FirstAPI {
 
-    next(): T | undefined;
-
-    prev(): T | undefined;
-
-    index(): number;
 }
-
-class PriorityTaskIterator implements IteratorInterface<Task> {
-
-    private position: number = 0;
-    private taskList: TaskList;
-
-    constructor(taskList: TaskList) {
-        taskList.sortByPriority()
-        this.taskList = taskList;
-    }
-
-    current(): Task | undefined {
-        return this.taskList.getTasks()[this.position];
-    }
-
-    next(): Task | undefined {
-        this.position += 1;
-        return this.taskList.getTasks()[this.position];
-    }
-
-    prev(): Task | undefined {
-        this.position -= 1;
-        return this.taskList.getTasks()[this.position];
-    }
-
-    index(): number {
-        return this.position;
-    }
-}
-
-const taskList = new TaskList()
-taskList.addTask(new Task(8))
-taskList.addTask(new Task(1))
-taskList.addTask(new Task(3))
-const iterator = taskList.getIterator()
-console.log(iterator.current())
-console.log(iterator.next())
-console.log(iterator.next())
-console.log(iterator.prev())
-console.log(iterator.index())
